a4aa311a4047cf70ea811a294e03711114c6ec6f
Jeremy Kitchen <kitchen@kitchen.io>
2019-12-06 22:53:24 -0800

    day 7 part 1
    separating this entirely from part2 because I'm going to change to
    a channel based input/output setup and I don't want to have to futz with
    all of my tests

---
+++ day7_part1/day7.go
@@ -0,0 +1,72 @@
+package day7
+
+import (
+	"fmt"
+	"math"
+)
+
+func Run(ops []int, pos int, input []int) ([]int, []int, error) {
+	// fmt.Printf("program (pos: %v): %v\n", pos, ops)
+	op := ops[pos] % 100
+
+	switch op {
+	case 1:
+		// fmt.Printf("%v = add %v %v -> %v\n", ops[pos:pos+4], Arg(ops, pos, 1), Arg(ops, pos, 2), ops[pos+3])
+		ops[ops[pos+3]] = Arg(ops, pos, 1) + Arg(ops, pos, 2)
+		pos += 4
+	case 2:
+		// fmt.Printf("%v = multi %v %v -> %v\n", ops[pos:pos+4], Arg(ops, pos, 1), Arg(ops, pos, 2), ops[pos+3])
+		ops[ops[pos+3]] = Arg(ops, pos, 1) * Arg(ops, pos, 2)
+		pos += 4
+	case 3:
+		// fmt.Printf("%v = input -> %v\n", ops[pos:pos+2], ops[pos+1])
+		ops[ops[pos+1]] = input[0]
+		input = input[1:]
+		pos += 2
+	case 4:
+		// fmt.Printf("%v = output %v\n", ops[pos:pos+2], Arg(ops, pos, 1))
+		input = append(input, Arg(ops, pos, 1))
+		pos += 2
+	case 5:
+		if Arg(ops, pos, 1) != 0 {
+			pos = Arg(ops, pos, 2)
+		} else {
+			pos += 3
+		}
+	case 6:
+		if Arg(ops, pos, 1) == 0 {
+			pos = Arg(ops, pos, 2)
+		} else {
+			pos += 3
+		}
+	case 7:
+		if Arg(ops, pos, 1) < Arg(ops, pos, 2) {
+			ops[ops[pos+3]] = 1
+		} else {
+			ops[ops[pos+3]] = 0
+		}
+		pos += 4
+	case 8:
+		if Arg(ops, pos, 1) == Arg(ops, pos, 2) {
+			ops[ops[pos+3]] = 1
+		} else {
+			ops[ops[pos+3]] = 0
+		}
+		pos += 4
+	case 99:
+		return ops, input, nil
+	default:
+		return nil, nil, fmt.Errorf("bad operation")
+	}
+
+	return Run(ops, pos, input)
+}
+
+func Arg(ops []int, pos int, arg int) int {
+	flags := ops[pos] / 100
+	argflag := (flags % int(math.Pow10(arg))) / int(math.Pow10(arg-1))
+	if argflag == 1 {
+		return ops[pos+arg]
+	}
+	return ops[ops[pos+arg]]
+}
---
+++ day7_part1/part1/main.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/kitchen/aoc2019/day7"
+)
+
+func main() {
+	var bestOutput int
+	var bestSettings []int
+
+	program := []int{3, 8, 1001, 8, 10, 8, 105, 1, 0, 0, 21, 30, 55, 76, 97, 114, 195, 276, 357, 438, 99999, 3, 9, 102, 3, 9, 9, 4, 9, 99, 3, 9, 1002, 9, 3, 9, 1001, 9, 5, 9, 1002, 9, 2, 9, 1001, 9, 2, 9, 102, 2, 9, 9, 4, 9, 99, 3, 9, 1002, 9, 5, 9, 1001, 9, 2, 9, 102, 5, 9, 9, 1001, 9, 4, 9, 4, 9, 99, 3, 9, 1001, 9, 4, 9, 102, 5, 9, 9, 101, 4, 9, 9, 1002, 9, 4, 9, 4, 9, 99, 3, 9, 101, 2, 9, 9, 102, 4, 9, 9, 1001, 9, 5, 9, 4, 9, 99, 3, 9, 1002, 9, 2, 9, 4, 9, 3, 9, 102, 2, 9, 9, 4, 9, 3, 9, 102, 2, 9, 9, 4, 9, 3, 9, 1001, 9, 1, 9, 4, 9, 3, 9, 102, 2, 9, 9, 4, 9, 3, 9, 1002, 9, 2, 9, 4, 9, 3, 9, 102, 2, 9, 9, 4, 9, 3, 9, 102, 2, 9, 9, 4, 9, 3, 9, 1001, 9, 2, 9, 4, 9, 3, 9, 101, 1, 9, 9, 4, 9, 99, 3, 9, 1002, 9, 2, 9, 4, 9, 3, 9, 1001, 9, 2, 9, 4, 9, 3, 9, 101, 1, 9, 9, 4, 9, 3, 9, 1002, 9, 2, 9, 4, 9, 3, 9, 1001, 9, 2, 9, 4, 9, 3, 9, 1001, 9, 2, 9, 4, 9, 3, 9, 1002, 9, 2, 9, 4, 9, 3, 9, 1001, 9, 2, 9, 4, 9, 3, 9, 101, 2, 9, 9, 4, 9, 3, 9, 102, 2, 9, 9, 4, 9, 99, 3, 9, 101, 1, 9, 9, 4, 9, 3, 9, 1002, 9, 2, 9, 4, 9, 3, 9, 1001, 9, 1, 9, 4, 9, 3, 9, 1002, 9, 2, 9, 4, 9, 3, 9, 1001, 9, 1, 9, 4, 9, 3, 9, 1001, 9, 1, 9, 4, 9, 3, 9, 101, 1, 9, 9, 4, 9, 3, 9, 102, 2, 9, 9, 4, 9, 3, 9, 1001, 9, 1, 9, 4, 9, 3, 9, 1001, 9, 1, 9, 4, 9, 99, 3, 9, 1001, 9, 1, 9, 4, 9, 3, 9, 1002, 9, 2, 9, 4, 9, 3, 9, 1001, 9, 2, 9, 4, 9, 3, 9, 1002, 9, 2, 9, 4, 9, 3, 9, 1001, 9, 1, 9, 4, 9, 3, 9, 101, 2, 9, 9, 4, 9, 3, 9, 102, 2, 9, 9, 4, 9, 3, 9, 101, 2, 9, 9, 4, 9, 3, 9, 101, 1, 9, 9, 4, 9, 3, 9, 1001, 9, 1, 9, 4, 9, 99, 3, 9, 101, 2, 9, 9, 4, 9, 3, 9, 102, 2, 9, 9, 4, 9, 3, 9, 101, 2, 9, 9, 4, 9, 3, 9, 101, 1, 9, 9, 4, 9, 3, 9, 101, 2, 9, 9, 4, 9, 3, 9, 1002, 9, 2, 9, 4, 9, 3, 9, 102, 2, 9, 9, 4, 9, 3, 9, 1001, 9, 1, 9, 4, 9, 3, 9, 1001, 9, 1, 9, 4, 9, 3, 9, 101, 2, 9, 9, 4, 9, 99}
+
+	firstIteration := true
+	for _, phaseSettings := range permutations([]int{0, 1, 2, 3, 4}) {
+		input := 0
+		for _, phaseSetting := range phaseSettings {
+			localProgram := make([]int, len(program))
+			copy(localProgram, program)
+			_, output, _ := day7.Run(localProgram, 0, []int{phaseSetting, input})
+			input = output[0]
+
+		}
+
+		if firstIteration || input > bestOutput {
+			bestSettings = phaseSettings
+			bestOutput = input
+		}
+
+		fmt.Printf("phaseSettings: %v, output: %v\n", phaseSettings, input)
+		firstIteration = false
+	}
+
+	fmt.Printf("best phase settings: %v, best output is: %v\n", bestSettings, bestOutput)
+}
+
+// thanks stackoverflow:
+// https://stackoverflow.com/questions/30226438/generate-all-permutations-in-go
+func permutations(arr []int) [][]int {
+	var helper func([]int, int)
+	res := [][]int{}
+
+	helper = func(arr []int, n int) {
+		if n == 1 {
+			tmp := make([]int, len(arr))
+			copy(tmp, arr)
+			res = append(res, tmp)
+		} else {
+			for i := 0; i < n; i++ {
+				helper(arr, n-1)
+				if n%2 == 1 {
+					tmp := arr[i]
+					arr[i] = arr[n-1]
+					arr[n-1] = tmp
+				} else {
+					tmp := arr[0]
+					arr[0] = arr[n-1]
+					arr[n-1] = tmp
+				}
+			}
+		}
+	}
+	helper(arr, len(arr))
+	return res
+}
