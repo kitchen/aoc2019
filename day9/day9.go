a3fe4fb426b9921dd41a9740276f56376454f9b2
Jeremy Kitchen <kitchen@kitchen.io>
2019-12-08 22:10:42 -0800

    removed some debugging and made Arg use ArgPos

--- day9/day9.go
+++ day9/day9.go
@@ -23,23 +23,23 @@ func Run(memory []int, pos int, input []int) ([]int, []int, error) {
 
 	go computer.Run()
 
-	fmt.Printf("pushing input\n")
+	// fmt.Printf("pushing input\n")
 	for _, inputValue := range input {
 		inputChan <- inputValue
 	}
 	close(inputChan)
 
-	fmt.Printf("pulling output\n")
+	// fmt.Printf("pulling output\n")
 	for outputValue := range outputChan {
-		fmt.Printf("pulled output\n")
+		// fmt.Printf("pulled output\n")
 		output = append(output, outputValue)
-		fmt.Printf("done pulled output\n")
+		// fmt.Printf("done pulled output\n")
 	}
 
-	fmt.Printf("waiting for done\n")
+	// fmt.Printf("waiting for done\n")
 	<-doneChan
 
-	fmt.Printf("done!\n")
+	// fmt.Printf("done!\n")
 	return computer.memory, output, nil
 }
 
@@ -50,35 +50,35 @@ func NewComputer(initMemory []int, initPos int, initRelative int, input chan int
 }
 
 func (c *Computer) Run() error {
-	fmt.Printf("program pos: %v\n", c.pos)
+	// fmt.Printf("program pos: %v\n", c.pos)
 	op := c.memory[c.pos] % 100
 
 	switch op {
 	case 1:
-		fmt.Printf("%v = add %v %v -> %v\n", c.memory[c.pos:c.pos+4], c.Arg(1), c.Arg(2), c.memory[c.pos+3])
+		// fmt.Printf("%v = add %v %v -> %v\n", c.memory[c.pos:c.pos+4], c.Arg(1), c.Arg(2), c.ArgPos(3))
 		c.memory[c.ArgPos(3)] = c.Arg(1) + c.Arg(2)
 		c.pos += 4
 	case 2:
-		fmt.Printf("%v = multi %v %v -> %v\n", c.memory[c.pos:c.pos+4], c.Arg(1), c.Arg(2), c.memory[c.pos+3])
+		// fmt.Printf("%v = multi %v %v -> %v\n", c.memory[c.pos:c.pos+4], c.Arg(1), c.Arg(2), c.ArgPos(3))
 		c.memory[c.ArgPos(3)] = c.Arg(1) * c.Arg(2)
 		c.pos += 4
 	case 3:
 		c.memory[c.ArgPos(1)] = <-c.input
-		fmt.Printf("%v = input (%v)-> %v\n", c.memory[c.pos:c.pos+2], c.Arg(1), c.ArgPos(1))
+		// fmt.Printf("%v = input (%v)-> %v\n", c.memory[c.pos:c.pos+2], c.Arg(1), c.ArgPos(1))
 		c.pos += 2
 	case 4:
-		fmt.Printf("%v = output %v\n", c.memory[c.pos:c.pos+2], c.Arg(1))
+		// fmt.Printf("%v = output %v\n", c.memory[c.pos:c.pos+2], c.Arg(1))
 		c.output <- c.Arg(1)
 		c.pos += 2
 	case 5:
-		fmt.Printf("%v = nzj %v -> %v / %v\n", c.memory[c.pos:c.pos+3], c.Arg(1), c.Arg(2), c.pos+3)
+		// fmt.Printf("%v = nzj %v -> %v / %v\n", c.memory[c.pos:c.pos+3], c.Arg(1), c.Arg(2), c.pos+3)
 		if c.Arg(1) != 0 {
 			c.pos = c.Arg(2)
 		} else {
 			c.pos += 3
 		}
 	case 6:
-		fmt.Printf("%v = zj %v -> %v / %v\n", c.memory[c.pos:c.pos+3], c.Arg(1), c.Arg(2), c.pos+3)
+		// fmt.Printf("%v = zj %v -> %v / %v\n", c.memory[c.pos:c.pos+3], c.Arg(1), c.Arg(2), c.pos+3)
 
 		if c.Arg(1) == 0 {
 			c.pos = c.Arg(2)
@@ -86,7 +86,7 @@ func (c *Computer) Run() error {
 			c.pos += 3
 		}
 	case 7:
-		fmt.Printf("%v = %v lt %v -> %v\n", c.memory[c.pos:c.pos+4], c.Arg(1), c.Arg(2), c.memory[c.pos+3])
+		// fmt.Printf("%v = %v lt %v -> %v\n", c.memory[c.pos:c.pos+4], c.Arg(1), c.Arg(2), c.ArgPos(3))
 
 		if c.Arg(1) < c.Arg(2) {
 			c.memory[c.ArgPos(3)] = 1
@@ -95,7 +95,7 @@ func (c *Computer) Run() error {
 		}
 		c.pos += 4
 	case 8:
-		fmt.Printf("%v = %v eq %v -> %v\n", c.memory[c.pos:c.pos+4], c.Arg(1), c.Arg(2), c.memory[c.pos+3])
+		// fmt.Printf("%v = %v eq %v -> %v\n", c.memory[c.pos:c.pos+4], c.Arg(1), c.Arg(2), c.ArgPos(3))
 		if c.Arg(1) == c.Arg(2) {
 			c.memory[c.ArgPos(3)] = 1
 		} else {
@@ -103,7 +103,7 @@ func (c *Computer) Run() error {
 		}
 		c.pos += 4
 	case 9:
-		fmt.Printf("%v = relative + %v -> %v\n", c.memory[c.pos:c.pos+2], c.Arg(1), c.relativeBase+c.Arg(1))
+		// fmt.Printf("%v = relative + %v -> %v\n", c.memory[c.pos:c.pos+2], c.Arg(1), c.relativeBase+c.Arg(1))
 		c.relativeBase += c.Arg(1)
 		c.pos += 2
 	case 99:
@@ -130,12 +130,5 @@ func (c *Computer) ArgPos(arg int) int {
 }
 
 func (c *Computer) Arg(arg int) int {
-	flags := c.memory[c.pos] / 100
-	argflag := (flags % int(math.Pow10(arg))) / int(math.Pow10(arg-1))
-	if argflag == 1 {
-		return c.memory[c.pos+arg]
-	} else if argflag == 2 {
-		return c.memory[c.relativeBase+c.memory[c.pos+arg]]
-	}
-	return c.memory[c.memory[c.pos+arg]]
+	return c.memory[c.ArgPos(arg)]
 }
